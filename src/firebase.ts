5143e5daf578f5c24bea6fc3910207f51c0d13c5
Ibrahim <ibrahim.hassan2402@gmail.com>
2026-02-15 18:00:00 +0000

    refactoring constants

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// Now pulling from Environment Variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCNHEG84tqeRPSu-BLO-ZwL9docOdzCrlA",
-  authDomain: "psvtr-study.firebaseapp.com",
-  projectId: "psvtr-study",
-  storageBucket: "psvtr-study.firebasestorage.app",
-  messagingSenderId: "931172214065",
-  appId: "1:931172214065:web:826d5fd19040130b5cfc10"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
